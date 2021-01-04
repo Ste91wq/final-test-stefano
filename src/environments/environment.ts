545933291e328c64ebb2fa1acfa47ed9dfd1f054
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-04 09:30:44 +0100

    Added api for jokes in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiChuckNorrisJokes: 'http://api.icndb.com/jokes/random'
 };
 
 /*
