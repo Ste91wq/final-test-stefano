8fe0655b02b0421db48b4c010821724c9c70763e
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-04 11:24:41 +0100

    Implemented getJokes method

--- src/app/shared/services/joke.service.ts
+++ src/app/shared/services/joke.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getJokes(): Observable<any> {
+    return this.http.get<any>(environment.apiChuckNorrisJokes)
+  }
 }
