201b00172eb6f1793bd6da36b0bd7b5e43ea68c5
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-04 12:56:14 +0100

    Refactoring code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { DslikedJokesComponent } from './core/dsliked-jokes/dsliked-jokes.component';
+import { FetchJokeComponent } from './core/fetch-joke/fetch-joke.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LikedJokesComponent } from './core/liked-jokes/liked-jokes.component';
 import { MaterialModule } from './shared/material/material.module';
 import { NgModule } from '@angular/core';
-import { FetchJokeComponent } from './core/fetch-joke/fetch-joke.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FetchJokeComponent
+    FetchJokeComponent,
+    LikedJokesComponent,
+    DslikedJokesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/fetch-joke/fetch-joke.component.ts
+++ src/app/core/fetch-joke/fetch-joke.component.ts
@@ -7,24 +7,22 @@ import { JokeService } from 'src/app/shared/services/joke.service';
 @Component({
   selector: 'app-fetch-joke',
   templateUrl: './fetch-joke.component.html',
-  styleUrls: ['./fetch-joke.component.css']
+  styleUrls: ['./fetch-joke.component.css'],
 })
 export class FetchJokeComponent implements OnInit {
   joke$: Observable<any>;
 
-  constructor(private jokeService: JokeService) { }
+  constructor(private jokeService: JokeService) {}
 
   ngOnInit(): void {
-    this.getJoke(30)
-    this.joke$.subscribe((joke) => console.log(joke));
+    this.getJoke(30);
   }
 
-  getJoke(int: number): void{
+  getJoke(int: number): void {
     this.joke$ = timer(0, int * 1000).pipe(
-      mergeMap(() => this.jokeService.getJokes().pipe(
-        map(val => val.value.joke)
-      ))
-    )
+      mergeMap(() =>
+        this.jokeService.getJokes().pipe(map((val) => val.value.joke))
+      )
+    );
   }
-
 }
