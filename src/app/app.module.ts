488be39e94345bf56ce9ddd9d7f2aae9ffd44184
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-04 09:50:19 +0100

    Added HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './shared/material/material.module';
 import { NgModule } from '@angular/core';
+import { FetchJokeComponent } from './core/fetch-joke/fetch-joke.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FetchJokeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaterialModule
   ],
   providers: [],
