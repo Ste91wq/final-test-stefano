bb3b16afbbb5dbf119c98bff84e6309ed1504c4a
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-10 12:10:27 +0100

    Created class Joke

--- src/app/shared/model/joke.ts
+++ src/app/shared/model/joke.ts
@@ -1,13 +1,31 @@
-export interface IJokedResponse {
-  type: string,
-  value: {
-    id: number,
-    joke: string
-  },
-  category: []
+import { JokeStatus } from "../enum/enums";
+
+export class Joke {
+  constructor(
+    private _id: number,
+    private _joke: string,
+    private _categories: Array<string>,
+    private _status = JokeStatus.UNASSIGNED
+  ) {}
+
+  get status(): JokeStatus {
+    return this._status;
+  }
+
+  set status(value: JokeStatus) {
+    this._status = value
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get joke(): string {
+    return this._joke;
+  }
+
+  get categories(): Array<string> {
+    return this._categories;
+  }
 }
 
-export interface IJoke {
-  id: number,
-  joke: string
-}
