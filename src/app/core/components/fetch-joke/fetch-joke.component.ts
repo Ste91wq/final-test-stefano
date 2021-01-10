3085b0dcb85d8f8d44be9109a00e5c8e46c43652
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-10 12:58:01 +0100

    Implemented fetch-joke.component with time 30 seconds

--- src/app/core/components/fetch-joke/fetch-joke.component.ts
+++ src/app/core/components/fetch-joke/fetch-joke.component.ts
@@ -1,13 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+import { Joke } from 'src/app/shared/model/joke';
+import { JokeService } from 'src/app/shared/services/joke.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fetch-joke',
   templateUrl: './fetch-joke.component.html',
   styleUrls: ['./fetch-joke.component.css'],
 })
-export class FetchJokeComponent implements OnInit {
+export class FetchJokeComponent implements OnInit, OnDestroy {
+  joke: Joke;
+  _fetchTime = 30;
 
-  constructor() {}
+  initialSubscription: Subscription;
+  jokeSubscription: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private jokeService: JokeService) {}
+
+  ngOnInit(): void {
+    this.initialSubscription = this.jokeService.getRandomJoke().subscribe(joke => this.joke = joke);
+    this.jokeSubscription = this.getJokeSubscription(this._fetchTime)
+  }
+
+  ngOnDestroy(): void {
+    this.initialSubscription.unsubscribe();
+    this.jokeUnsuscribe();
+  }
+
+  get fetchTime(): number {
+    return this._fetchTime;
+  }
+
+  set fetchTime(time: number) {
+    this._fetchTime = time;
+    this.jokeUnsuscribe();
+    this.jokeSubscription = this.getJokeSubscription(time);
+  }
+
+  private getJokeSubscription(time: number): Subscription {
+    return interval(time * 1000).pipe(
+      switchMap(() => this.jokeService.getRandomJoke())
+    ).subscribe(joke => this.joke = joke);
+  }
+
+  private jokeUnsuscribe(): void {
+    if (this.jokeSubscription) {
+      this.jokeSubscription.unsubscribe();
+    }
+  }
 }
