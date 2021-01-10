51e5da42ea7829bad42d573246f0b1080d2fdf21
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-10 12:10:00 +0100

    Import module in material module

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,8 +1,10 @@
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 
-const modules = [MatButtonModule, MatInputModule];
+const modules = [MatButtonModule, MatInputModule, MatCardModule, MatIconModule];
 
 @NgModule({
   declarations: [],
