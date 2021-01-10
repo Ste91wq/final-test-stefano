76a86905b430521d4c8973fe011527d2a3da7b66
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-10 12:11:06 +0100

    Created enum JokeStatus, VoteType and CardContainerType

--- src/app/shared/enum/enums.ts
+++ src/app/shared/enum/enums.ts
@@ -1,6 +1,17 @@
-export enum Type {
+export enum JokeStatus {
+  UNASSIGNED = 'unassigned',
   LIKED = 'liked',
   DISLIKED = 'disliked',
-  ARCHIVED = 'archived',
-  DELETE = 'delete'
+  ARCHIVED = 'archived'
+}
+
+export enum VoteType {
+  LIKE = 'like',
+  DISLIKE = 'dislike'
+}
+
+export enum CardContainerType {
+  LIKED = 'liked',
+  DISLIKED = 'disliked',
+  ARCHIVED = 'archived'
 }
