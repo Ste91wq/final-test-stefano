c176b31eaa74bf180588c0ee4b0cd32c1234cdb0
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-10 16:38:55 +0100

    Implemented logic liked, disliked, archived and deleted in container

--- src/app/core/components/container/container.component.ts
+++ src/app/core/components/container/container.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CardContainerType } from 'src/app/shared/enum/enums';
+import { Joke } from 'src/app/shared/model/joke';
+import { JokeService } from 'src/app/shared/services/joke.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
 
-  constructor() { }
+  CardContainerType = CardContainerType;
+
+  likedJokes$: Observable<Joke[]>;
+  dislikedJokes$: Observable<Joke[]>;
+  archivedJokes$: Observable<Joke[]>;
+
+  constructor(private jokeService: JokeService) { }
 
   ngOnInit(): void {
+    this.likedJokes$ = this.jokeService.getAllLikedJokes();
+    this.dislikedJokes$ = this.jokeService.getAllDisLikedJokes();
+    this.archivedJokes$ = this.jokeService.getAllArchivedJokes();
+  }
+
+  onJokeLiked(joke: Joke): void {
+    this.jokeService.likeJoke(joke);
+  }
+
+  onJokeDisliked(joke: Joke): void {
+    this.jokeService.dislikeJoke(joke);
+  }
+
+  onJokeArchived(joke: Joke): void {
+    this.jokeService.archive(joke);
+  }
+
+  onJokeDelete(joke: Joke): void {
+    this.jokeService.delete(joke);
   }
 
 }
