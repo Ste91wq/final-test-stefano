a95578c98563566e30751e613ae737010c6136b3
Stefano Rizzo <stefanorizzo295@gmail.com>
2021-01-10 16:39:26 +0100

    Implemented logic in mat-card-joke component

--- src/app/core/components/mat-card-joke/mat-card-joke.component.ts
+++ src/app/core/components/mat-card-joke/mat-card-joke.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { CardContainerType } from 'src/app/shared/enum/enums';
+import { Joke } from 'src/app/shared/model/joke';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-mat-card-joke',
   templateUrl: './mat-card-joke.component.html',
-  styleUrls: ['./mat-card-joke.component.css']
+  styleUrls: ['./mat-card-joke.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MatCardJokeComponent implements OnInit {
+
+  @Input() cardTitle: string;
+  @Input() jokes$: Observable<Array<Joke>>;
+  @Input() containerType: CardContainerType;
+
+  @Output() liked = new EventEmitter<Joke>();
+  @Output() disliked = new EventEmitter<Joke>();
+  @Output() archived = new EventEmitter<Joke>();
+  @Output() deleted = new EventEmitter<Joke>();
+
+  CardContainerType = CardContainerType;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  like(joke: Joke): void {
+    this.liked.emit(joke);
+  }
+
+  dislike(joke: Joke): void {
+    this.disliked.emit(joke);
+  }
+
+  archive(joke: Joke): void {
+    this.archived.emit(joke);
+  }
+
+  delete(joke: Joke): void {
+    this.deleted.emit(joke);
   }
 
 }
